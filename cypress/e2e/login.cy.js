ebdeb0de7aa2c0a35338e09385ae322cc835df63
Larissa Cabral <oliveiralari09@outlook.com>
2026-07-28 13:35:11 -0300

    add os arquivos iniciais e teste de login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+describe('login', () => {
+  it('Login com dados validos deve permitir entrada no sistema', () => {
+    //arrange
+    cy.visit('http://localhost:4000')
+    //act
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('123456')
+    cy.get('#login-section > .btn').click()
+    //assert
+    cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+})
