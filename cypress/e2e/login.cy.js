c2b19fbdb2e625350907c5a66efacbddb5bd4d93
Larissa Cabral <oliveiralari09@outlook.com>
2026-07-30 15:00:10 -0300

    add fixtures

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,8 +7,10 @@ describe('login', () => {
   it('Login com dados validos deve permitir entrada no sistema', () => {
 
     //act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then(credenciais =>{
+        cy.get('#username').click().type(credenciais.valida.usuario)
+        cy.get('#senha').click().type(credenciais.valida.senha)
+    })
     cy.screenshot('apos-preencher-dados')
     cy.contains('button', 'Entrar').click()
     cy.screenshot('apos-clicar-no-botao-entrar')
@@ -18,8 +20,10 @@ describe('login', () => {
 
   it('Login com dados invalidos deve apresentar mensagem de erro', () => {
     //act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('654321')
+    cy.fixture('credenciais').then(credenciais =>{
+        cy.get('#username').click().type(credenciais.invalida.usuario)
+        cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
     cy.contains('button', 'Entrar').click()
     //assert
     cy.get('.toast').should('be.visible').should('have.text', 'Erro no login. Tente novamente.')
